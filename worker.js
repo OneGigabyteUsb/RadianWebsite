9e16efe9440a251a1f380872d31cf781bc03ce09
OneGigabyteUsb <onegigabyteusb@gmail.com>
2026-08-13 02:30:12 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,12 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/api/test-db") {
-      const result = await env.DB
-        .prepare("SELECT name FROM sqlite_master WHERE type='table'")
-        .all();
-
-      return Response.json(result);
+    if (url.pathname === "/api/test") {
+      return new Response("WORKER IS WORKING!", {
+        headers: {
+          "Content-Type": "text/plain"
+        }
+      });
     }
 
     return env.ASSETS.fetch(request);
