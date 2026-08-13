ee3002ee1e04f23298363ac4925e1fd6be89ea23
OneGigabyteUsb <onegigabyteusb@gmail.com>
2026-08-13 02:22:32 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,15 @@
-console.log("boi")
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/test-db") {
+      const result = await env.DB
+        .prepare("SELECT name FROM sqlite_master WHERE type='table'")
+        .all();
+
+      return Response.json(result);
+    }
+
+    return env.ASSETS.fetch(request);
+  }
+};
