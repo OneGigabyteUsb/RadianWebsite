22e88f0e2036ef0acf46c4abc79f1b94e9f0f20f
OneGigabyteUsb <onegigabyteusb@gmail.com>
2026-08-22 16:32:48 -0400

    Update main.js

--- website/play/main.js
+++ website/play/main.js
@@ -203,7 +203,7 @@ function appendChatMessage(username, text) {
 
 //=====Player Stuff=====\\
 let JumpPower = 0.54;
-let WalkSpeed = -0.28;
+let WalkSpeed = -0.48;
 let spawn = new THREE.Vector3();
 
 //=====Movement feel (accel/friction model, coyote time, jump buffer)=====\\
