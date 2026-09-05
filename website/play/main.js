ee0d529c67106369024c6931e6d6180003c5653c
OneGigabyteUsb <onegigabyteusb@gmail.com>
2026-09-05 01:49:56 -0400

    Update main.js

--- website/play/main.js
+++ website/play/main.js
@@ -555,7 +555,7 @@ sun.castShadow = true;
 sun.shadow.normalBias = 0.02;
 sun.shadow.camera = new THREE.OrthographicCamera(-50, 50, 50, -50, 0.5, 500);
 sun.shadow.mapSize.set(2048, 2048);
-sun.shadow.camera. = -30;
+sun.shadow.camera.left = -30;
 sun.shadow.camera.right = 30;
 sun.shadow.camera.top = 30;
 sun.shadow.camera.bottom = -30;
