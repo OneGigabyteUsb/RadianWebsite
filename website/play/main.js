74abb7c131d5566eedaa2ff565472b09226fea60
OneGigabyteUsb <onegigabyteusb@gmail.com>
2026-09-06 22:50:52 -0400

    Update main.js

--- website/play/main.js
+++ website/play/main.js
@@ -1097,7 +1097,7 @@ function networkSetPart(name, props) {
     }
 }
 
-const DYNAMIC_SYNC_RATE = 1 / 20;
+const DYNAMIC_SYNC_RATE = 1 / 200;
 let lastDynamicSync = 0;
 
 function sendDynamicPartsSync(elapsedSeconds) {
@@ -1257,7 +1257,7 @@ function interpolateOtherPlayers(deltaSeconds) {
     }
 }
 
-const MULTIPLAYER_SEND_RATE = 1 / 20; // matches TICK_RATE in multiplayer.py
+const MULTIPLAYER_SEND_RATE = 1 / 200; // matches TICK_RATE in multiplayer.py
 let lastMultiplayerSend = 0;
 
 function sendMyPosition(elapsedSeconds) {
